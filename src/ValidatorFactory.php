b64f07e8d371809ff68e9fe724e6d94ddb1c8999
mouyong <mouyong@users.noreply.github.com>
2022-04-30 14:13:20 +0800

    Update ValidatorFactory.php

--- src/ValidatorFactory.php
+++ src/ValidatorFactory.php
@@ -12,22 +12,43 @@ use Illuminate\Translation\Translator;
  * @mixin Factory
  *
  * @see https://medium.com/@jeffochoa/using-the-illuminate-validation-validator-class-outside-laravel-6b2b0c07d3a4
+ * @see https://github.com/mouyong/validate/blob/master/src/ValidatorFactory.php
  */
 class ValidatorFactory
 {
+    protected $langPath;
+    
     /**
      * @var Factory
      */
     private $factory;
 
-    public function __construct()
+    public function __construct(?string $langPath = null)
     {
+        $this->setLangPath($langPath);
+
         $this->factory = new Factory($this->loadTranslator());
+    }
+
+    public function setLangPath(?string $langPath = null)
+    {
+        $this->langPath = $langPath;
+    }
+
+    public function getLangPath()
+    {
+        if (is_null($this->langPath) && !class_exists(\Illuminate\Foundation\Application::class)) {
+            $this->langPath = base_path() . '/resource/translations';
+        } else if (is_null($this->langPath) && class_exists(\Illuminate\Foundation\Application::class)) {
+            $this->langPath = base_path() . '/resources/lang';
+        }
+
+        return $this->langPath;
     }
 
     protected function loadTranslator(): Translator
     {
-        $langPath = base_path() . '/resource/translations';
+        $langPath = $this->getLangPath();
 
         $filesystem = new Filesystem();
         $loader = new Translation\FileLoader($filesystem, $langPath);
